344dd298ec201f1d7818d5a7d4317ccbf18652ba
srestrero <srestreporo@unal.edu.co>
2025-06-08 23:01:29 -0500

    feat(main): add departemnts routes to principal app

---
+++ src/pae_cobertura/main.py
@@ -0,0 +1,16 @@
+# pae_cobertura/main.py
+from fastapi import FastAPI
+from pae_cobertura.routes.departments import router as departments_router
+
+app = FastAPI(
+    title="API de Cobertura del PAE",
+    description="Sistema para gestionar la estructura geográfica y de beneficiarios del PAE.",
+    version="1.0.0"
+)
+
+# Incluir los routers de los endpoints
+app.include_router(departments_router, prefix="/api/v1/departments", tags=["Departments"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the PAE Coverage API"}
